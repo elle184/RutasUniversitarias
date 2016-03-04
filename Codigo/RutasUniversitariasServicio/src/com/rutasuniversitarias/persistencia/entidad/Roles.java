28830da6e41f5ad920b2831d5af3eab8176dd8bc
Adrian Becerra <shogunato184@gmail.com>
2016-03-04 10:16:57 -0500

    Creación de codigo fuente del servicio WEB.

---
+++ Codigo/RutasUniversitariasServicio/src/com/rutasuniversitarias/persistencia/entidad/Roles.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rutasuniversitarias.persistencia.entidad;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author abecerra
+ */
+@Entity
+@Table(name = "roles")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Roles.findAll", query = "SELECT r FROM Roles r"),
+    @NamedQuery(name = "Roles.findByIdRol", query = "SELECT r FROM Roles r WHERE r.idRol = :idRol"),
+    @NamedQuery(name = "Roles.findByNombreRol", query = "SELECT r FROM Roles r WHERE r.nombreRol = :nombreRol")})
+public class Roles implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_rol")
+    private Integer idRol;
+    @Column(name = "nombre_rol")
+    private String nombreRol;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "rolesIdRol")
+    private List<Personas> personasList;
+
+    public Roles() {
+    }
+
+    public Roles(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    @XmlTransient
+    public List<Personas> getPersonasList() {
+        return personasList;
+    }
+
+    public void setPersonasList(List<Personas> personasList) {
+        this.personasList = personasList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idRol != null ? idRol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Roles)) {
+            return false;
+        }
+        Roles other = (Roles) object;
+        if ((this.idRol == null && other.idRol != null) || (this.idRol != null && !this.idRol.equals(other.idRol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.rutasuniversitarias.persistencia.entidad.Roles[ idRol=" + idRol + " ]";
+    }
+    
+}
